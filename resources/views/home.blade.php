c5cb5beb1974ac7efce1aa1f0fe241b930a99e31
SStalker <raphael@bluepanel.org>
2016-11-09 23:54:30 +0100

    Left menu

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('leftContent')
+<a href="google.de" class="mybutton">Google</a>
+@endSection
+
+@section('middleContent')
+Inhalt der in der Mitte stehen sollte
+@endsection
